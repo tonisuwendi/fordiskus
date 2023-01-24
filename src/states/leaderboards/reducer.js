7fac4d64bc43ce30c136f8371420169932b26031
Toni Suwendi <tonisuwendi2002@gmail.com>
2023-01-24 11:42:40 +0700

    make test error

--- src/states/leaderboards/reducer.js
+++ src/states/leaderboards/reducer.js
@@ -3,7 +3,7 @@ import { ActionType } from './action';
 const leaderboardsReducer = (leaderboards = [], action = {}) => {
     switch (action.type) {
     case ActionType.RECEIVE_LEADERBOARDS:
-        return action.payload.leaderboards;
+        return action.payload;
     default:
         return leaderboards;
     }
